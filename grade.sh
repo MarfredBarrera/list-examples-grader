765c8fdd1fb4cf7d40697a0d5201a09d89f2ac21
MarfredBarrera <mrbarrera@ucsd.edu>
2023-05-24 17:20:00 -0700

    fixed

--- grade.sh
+++ grade.sh
@@ -1,4 +1,4 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH=".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar"
 
 rm -rf student-submission
 rm -rf grading-area
@@ -15,7 +15,8 @@ if [[ -f "$path" ]];
 then
     echo "ListExamples.java found"
 else
-    echo "ListExamples.java not found"
+    echo "ListExamples.java not found. Here are the files we see: "
+    ls student-submission
     exit
 fi
 
@@ -26,12 +27,13 @@ javac -cp $CPATH *.java
 
 if [[ $? = 1 ]]; 
 then
-    echo "cannot compile"
+    echo "Compile failed! See above for error message"
     exit
 else
-    echo "compiled successfully"
+    echo "Compiled successfully!"
 fi
 
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
 
 
 # Draw a picture/take notes on the directory structure that's set up after
--- grading-area/TestListExamples.java
+++ grading-area/TestListExamples.java
@@ -18,4 +18,13 @@ public class TestListExamples {
     List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
     assertEquals(expected, merged);
   }
+
+  @Test(timeout = 500)
+  public void testFilter() {
+
+    StringChecker sc = new IsMoon();
+    List<String> unfiltered = Arrays.asList("moon","sun","abc", "banana");
+    List<String> filtered = Arrays.asList("moon");
+    assertEquals(filtered, ListExamples.filter(unfiltered, sc));
+  }
 }
